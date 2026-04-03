7044655b9c310c5f1bab543be972ee17c9e58a04
Fihlter <sorty7900@gmail.com>
2026-04-02 21:11:15 -0400

    Made player a directional arrow and now points to players mouse cursor

--- core/src/main/java/com/nomuse/freecell/game/ExtractionGame.java
+++ core/src/main/java/com/nomuse/freecell/game/ExtractionGame.java
@@ -7,6 +7,9 @@ import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Matrix4;
+import com.badlogic.gdx.math.Vector3;
 
 public class ExtractionGame extends ApplicationAdapter {
 
@@ -17,6 +20,7 @@ public class ExtractionGame extends ApplicationAdapter {
     private float playerX = 400;
     private float playerY = 400;
     private float playerSpeed = 200f;
+    private float playerAngle = 0f;
 
     @Override
     public void create() {
@@ -39,18 +43,41 @@ public class ExtractionGame extends ApplicationAdapter {
 
         mapManager.render(shapeRenderer);
 
+        // Render player
+        Matrix4 oldTransform = new Matrix4(shapeRenderer.getTransformMatrix());
+
+        // Create new transform matrix
+        Matrix4 transform = new Matrix4();
+        transform.setToTranslation(playerX, playerY, 0);
+        transform.rotate(0, 0, 1, playerAngle);
+        shapeRenderer.setTransformMatrix(transform);
+
         shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
         shapeRenderer.setColor(Color.BLUE);
-        shapeRenderer.rect(playerX, playerY, 24, 24);
+        shapeRenderer.triangle(15, 0, -12, -12, -12, 12);
         shapeRenderer.end();
+
+        shapeRenderer.setTransformMatrix(oldTransform);
     }
 
     private void handleInput() {
         float deltaTime = Gdx.graphics.getDeltaTime();
+
+        // Movement input
         if (Gdx.input.isKeyPressed(Input.Keys.W)) playerY += playerSpeed * deltaTime;
         if (Gdx.input.isKeyPressed(Input.Keys.S)) playerY -= playerSpeed * deltaTime;
         if (Gdx.input.isKeyPressed(Input.Keys.A)) playerX -= playerSpeed * deltaTime;
         if (Gdx.input.isKeyPressed(Input.Keys.D)) playerX += playerSpeed * deltaTime;
+
+        // Mouse aiming input
+        Vector3 mousePos = new Vector3(Gdx.input.getX(), Gdx.input.getY(), 0);
+
+        camera.unproject(mousePos);
+
+        float dx = mousePos.x - playerX;
+        float dy = mousePos.y - playerY;
+
+        playerAngle = MathUtils.radiansToDegrees * MathUtils.atan2(dy, dx);
     }
 
     private void updateCamera() {
