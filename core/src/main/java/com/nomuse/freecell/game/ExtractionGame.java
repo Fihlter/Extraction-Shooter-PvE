2adb39fcf2e3d115a8cb15203a22789fa3ec23d8
Fihlter <sorty7900@gmail.com>
2026-04-03 02:12:43 -0400

    Working on collision

--- core/src/main/java/com/nomuse/freecell/game/ExtractionGame.java
+++ core/src/main/java/com/nomuse/freecell/game/ExtractionGame.java
@@ -13,13 +13,16 @@ import com.badlogic.gdx.graphics.g3d.Model;
 import com.badlogic.gdx.graphics.g3d.ModelBatch;
 import com.badlogic.gdx.graphics.g3d.ModelInstance;
 import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
-import com.badlogic.gdx.graphics.g3d.attributes.TextureAttribute;
+import com.badlogic.gdx.graphics.g3d.attributes.FloatAttribute;
 import com.badlogic.gdx.graphics.g3d.environment.DirectionalLight;
+import com.badlogic.gdx.graphics.g3d.environment.DirectionalShadowLight;
 import com.badlogic.gdx.graphics.g3d.loader.ObjLoader;
+import com.badlogic.gdx.graphics.g3d.utils.DepthShaderProvider;
 import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.math.collision.BoundingBox;
 
 public class ExtractionGame extends ApplicationAdapter {
     
@@ -27,6 +30,9 @@ public class ExtractionGame extends ApplicationAdapter {
     private ModelBatch modelBatch;
     private Environment environment;
     private MapManager mapManager;
+
+    private ModelBatch shadowBatch;
+    private DirectionalShadowLight shadowLight;
 
     // Network player state
     private PlayerEntity localPlayer;
@@ -36,9 +42,9 @@ public class ExtractionGame extends ApplicationAdapter {
     // Melee weapon visuals
     private Model swordModel;
     private ModelInstance swordInstance;
-    private Texture swordTexture;
 
     private float swordScale = 0.0025f;
+    private float hiltOffsetY = 0f;
 
     @Override
     public void create() {
@@ -55,12 +61,20 @@ public class ExtractionGame extends ApplicationAdapter {
 
         // Setup 3D renderer and model builder
         modelBatch = new ModelBatch();
+
+        shadowBatch = new ModelBatch(new DepthShaderProvider());
+
         ModelBuilder modelBuilder = new ModelBuilder();
 
         // Setup lighting
         environment = new Environment();
         environment.set(new ColorAttribute(ColorAttribute.AmbientLight, 0.4f, 0.4f, 0.4f, 1f));
-        environment.add(new DirectionalLight().set(0.8f, 0.8f, 0.8f, -1f, -0.8f, -0.2f));
+        
+        shadowLight = new DirectionalShadowLight(4096, 4096, 45f, 45f, 1f, 300f);
+        shadowLight.set(0.8f, 0.8f, 0.8f, -1f, -0.8f, -0.2f);
+
+        environment.add(shadowLight);
+        environment.shadowMap = shadowLight;
 
         mapManager = new MapManager(modelBuilder);
 
@@ -69,8 +83,14 @@ public class ExtractionGame extends ApplicationAdapter {
         swordModel = loader.loadModel(Gdx.files.internal("sword/kamasword.obj"));
         swordInstance = new ModelInstance(swordModel);
 
-        swordTexture = new Texture(Gdx.files.internal("sword/kamasword_Diff_Metal.jpg"));
-        swordInstance.materials.get(0).set(TextureAttribute.createDiffuse(swordTexture));
+        BoundingBox bounds = new BoundingBox();
+        swordInstance.calculateBoundingBox(bounds);
+        hiltOffsetY = -bounds.min.y;
+
+        swordInstance.materials.get(0).clear();
+        swordInstance.materials.get(0).set(ColorAttribute.createDiffuse(Color.DARK_GRAY));
+        swordInstance.materials.get(0).set(ColorAttribute.createSpecular(Color.WHITE));
+        swordInstance.materials.get(0).set(FloatAttribute.createShininess(100f));
     }
 
     @Override
@@ -78,10 +98,18 @@ public class ExtractionGame extends ApplicationAdapter {
         float delta = Gdx.graphics.getDeltaTime();
 
         handleInput(delta);
-
         localPlayer.update(delta);
-
         updateCameraAndWeapon();
+
+        shadowLight.begin(camera.position, camera.direction);
+        shadowBatch.begin(shadowLight.getCamera());
+
+        for (ModelInstance block : mapManager.blocks) {
+            shadowBatch.render(block);
+        }
+
+        shadowBatch.end();
+        shadowLight.end();
 
         // Clear screen to sky blue
         ScreenUtils.clear(0.5f, 0.8f, 1f, 1f, true);
@@ -120,6 +148,9 @@ public class ExtractionGame extends ApplicationAdapter {
         Vector3 forward = new Vector3((float)Math.sin(Math.toRadians(localPlayer.yaw)), 0, (float)Math.cos(Math.toRadians(localPlayer.yaw))).nor();
         Vector3 right = new Vector3(forward).crs(Vector3.Y).nor();
 
+        float dx = 0;
+        float dz = 0;
+
         if (Gdx.input.isKeyPressed(Input.Keys.W)) {
             localPlayer.x -= forward.x * moveSpeed * deltaTime;
             localPlayer.z -= forward.z * moveSpeed * deltaTime;
@@ -137,12 +168,21 @@ public class ExtractionGame extends ApplicationAdapter {
             localPlayer.z -= right.z * moveSpeed * deltaTime;
         }
 
+        float playerRadius = 0.4f;
+
+        if (!mapManager.isColliding(localPlayer.x + dx, localPlayer.z, playerRadius)) {
+            localPlayer.x += dx;
+        }
+
+        if (!mapManager.isColliding(localPlayer.x, localPlayer.z + dz, playerRadius)) {
+            localPlayer.z += dz;
+        }
+
         // Melee attack
         if (Gdx.input.isButtonJustPressed(Input.Buttons.LEFT) && !localPlayer.isAttacking) {
             localPlayer.isAttacking = true;
             localPlayer.attackTimer = 0f;
         }
-
     }
 
     private void updateCameraAndWeapon() {
@@ -159,29 +199,32 @@ public class ExtractionGame extends ApplicationAdapter {
         swordInstance.transform.rotate(Vector3.Y, localPlayer.yaw);
         swordInstance.transform.rotate(Vector3.X, localPlayer.pitch);
 
+        swordInstance.transform.translate(0.5f, -0.3f, -0.8f);
+
+        if (localPlayer.isAttacking) {
+            float progress = localPlayer.attackTimer / PlayerEntity.ATTACK_DURATION;
+            float swingAngle = MathUtils.sin((float) (progress * Math.PI)) * 90f; 
+            swordInstance.transform.rotate(Vector3.X, -swingAngle);
+            swordInstance.transform.rotate(Vector3.Z, swingAngle * 0.2f);
+        }
+
+        swordInstance.transform.scale(swordScale, swordScale, swordScale);
+
+        swordInstance.transform.translate(0, hiltOffsetY, 0);
+
         // Offset sword
         swordInstance.transform.translate(0.75f, -0.15f, -0.8f);
 
-        //swordInstance.transform.rotate(Vector3.X, 15f);
-        //swordInstance.transform.rotate(Vector3.Z, 15f);
-
-        swordInstance.transform.scale(swordScale, swordScale, swordScale);
+        
         swordInstance.transform.rotate(Vector3.Y, 90f);
         swordInstance.transform.rotate(Vector3.Z, -90f);
-
-        // Animate sword swing
-        if (localPlayer.isAttacking) {
-            float progress = localPlayer.attackTimer / PlayerEntity.ATTACK_DURATION;
-            // Swing down and to the left
-            float swingAngle = MathUtils.sin((float) (progress * Math.PI)) * 100f;
-            swordInstance.transform.rotate(Vector3.Z, -swingAngle);
-            swordInstance.transform.rotate(Vector3.X, -swingAngle * 0.3f);
-        }
     }
 
     @Override
     public void dispose() {
         modelBatch.dispose();
+        shadowBatch.dispose();
+        shadowLight.dispose();
         mapManager.dispose();
         swordModel.dispose();
     }
--- core/src/main/java/com/nomuse/freecell/game/MapManager.java
+++ core/src/main/java/com/nomuse/freecell/game/MapManager.java
@@ -7,6 +7,7 @@ import com.badlogic.gdx.graphics.g3d.Model;
 import com.badlogic.gdx.graphics.g3d.ModelInstance;
 import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
 import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder;
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.utils.Array;
 
 import java.util.Random;
@@ -19,6 +20,9 @@ import java.util.Random;
 public class MapManager {
     // Build all 3D blocks in the world
     public Array<ModelInstance> blocks = new Array<>();
+
+    // Collisioin grid
+    public boolean[][] solidGrid = new boolean[20][20];
 
     private Model floorModel;
     private Model wallModel;
@@ -43,20 +47,53 @@ public class MapManager {
         // Generate a 20x20 grid
         for (int x = -10; x < 10; x++) {
             for (int z = -10; z < 10; z++) {
-                // Place floor block at Y = 0
+                int gridX = x + 10;
+                int gridZ = z + 10;
+
+                solidGrid[gridX][gridZ] = false;
+
                 ModelInstance floor = new ModelInstance(floorModel);
                 floor.transform.setToTranslation(x * 2f, 0f, z * 2f);
                 blocks.add(floor);
 
-                // 20% chance to place wall block above the floor at Y = 2
-                if (rand.nextFloat() < 0.2f) {
+                boolean isSpawnArea = (x >= -1 && x <= 1) && (z >= 1 && z <= 3);
+
+                if (!isSpawnArea && rand.nextFloat() < 0.2f) {
                     ModelInstance wall = new ModelInstance(wallModel);
                     wall.transform.setToTranslation(x * 2f, 2f, z * 2f);
                     blocks.add(wall);
+
+                    solidGrid[gridX][gridZ] = true;
                 }
             }
         }
     }
+
+    public boolean isColliding(float worldX, float worldZ, float playerRadius) {
+        float minX = worldX - playerRadius;
+        float maxX = worldX + playerRadius;
+        float minZ = worldZ - playerRadius;
+        float maxZ = worldZ + playerRadius;
+
+        int startX = MathUtils.floor((minX + 1f) / 2f) + 10;
+        int endX = MathUtils.floor((maxX + 1f) / 2f) + 10;
+        int startZ = MathUtils.floor((minZ + 1f) / 2f) + 10;
+        int endZ = MathUtils.floor((maxZ + 1f) / 2f) + 10;
+
+        for (int gx = startX; gx <= endX; gx++) {
+            for (int gz = startZ; gz <= endZ; gz++) {
+                if (gx < 0 || gx >= 20 || gz < 0 || gz >= 20) {
+                    return true;
+                }
+
+                if (solidGrid[gx][gz]) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
 
     public void dispose() {
         floorModel.dispose();
